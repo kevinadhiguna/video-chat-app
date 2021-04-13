36f4d574cd448af15d1bcb060c37cc8e5f1a0a5c
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-13 09:50:24 +0700

    refactor: add new functions

--- public/script.js
+++ public/script.js
@@ -12,12 +12,27 @@ const myVideo = document.createElement('video');
 // Mute the user's voice itself
 myVideo.muted = true;
 
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true,
+}).then(stream => {
+  addVideoStream(myVideo, stream);
+
+  socket.on('user-connected', userId => {
+    connectToNewUser(userId, stream);
+  });
+});
+
 myPeer.on('open', id => {
   socket.emit('join-room', ROOM_ID, id);
 });
 
-socket.emit('join-room', ROOM_ID, 10);
+socket.emit('join-room', ROOM_ID, id);
 
-socket.on('user-connected', userId => {
-  console.log('A user connected :', userId);
-});
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener('loadedmetadata', () => {
+    video.play();
+  });
+  videoGrid.append(video);
+}

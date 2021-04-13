d26fb846d5e8f5843a9b31c3e94967f4f9d431ae
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-13 09:29:06 +0700

    refactor: add event on open Peer

--- public/script.js
+++ public/script.js
@@ -4,6 +4,10 @@ const myPeer = new Peer(undefined, {
   port: '3001'
 });
 
+myPeer.on('open', id => {
+  socket.emit('join-room', ROOM_ID, id);
+});
+
 socket.emit('join-room', ROOM_ID, 10);
 socket.on('user-connected', userId => {
   console.log('A user connected :', userId);

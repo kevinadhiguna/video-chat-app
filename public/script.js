9189e09b2ae52ba079d2ecfe1a18f525a96d27cd
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-13 10:17:27 +0700

    refactor: add addVideoStream function

--- public/script.js
+++ public/script.js
@@ -47,6 +47,10 @@ function connectToNewUser(userId, stream) {
   const call = myPeer.call(userId, stream);
   const video = document.createElement('video');
   
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream);
+  });
+
   call.on('close', () => {
     video.remove();
   });

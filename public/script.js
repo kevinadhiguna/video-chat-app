066227340b7f596bdc008fba59e84bea30506ad5
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-13 09:32:27 +0700

    refactor: initialize videoGrid and mute user's voice

--- public/script.js
+++ public/script.js
@@ -1,14 +1,23 @@
 const socket = io('/');
+
+const videoGrid = document.getElementById('video-grid');
+
 const myPeer = new Peer(undefined, {
   host: '/',
   port: '3001'
 });
+
+const myVideo = document.createElement('video');
+
+// Mute the user's voice itself
+myVideo.muted = true;
 
 myPeer.on('open', id => {
   socket.emit('join-room', ROOM_ID, id);
 });
 
 socket.emit('join-room', ROOM_ID, 10);
+
 socket.on('user-connected', userId => {
   console.log('A user connected :', userId);
 });
